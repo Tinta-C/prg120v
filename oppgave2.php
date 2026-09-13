e497e29f5cd45f9ee0497a6778bb2a948efcbb3f
Tinta-C <mucha4460@usn.no>
2026-09-13 23:35:56 +0200

    Add files via upload

---
+++ oppgave2.php
@@ -0,0 +1,12 @@
+<?php
+$fornavn = $_POST['fornavn'];
+$etternavn = $_POST['etternavn'];
+$alder = $_POST['alder'];
+echo "God dag, $fornavn $etternavn, du er $alder år og like sprek.";
+
+?>
+
+
+
+
+
