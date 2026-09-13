0e5e45a6e9c5b581b6995867195554f6538d4671
Tinta-C <mucha4460@usn.no>
2026-09-14 00:03:01 +0200

    Add files via upload

---
+++ oppgave3.php
@@ -0,0 +1,12 @@
+<?php
+$tall1 = $_POST['tall1'];
+$tall2 = $_POST['tall2'];
+$sum = $tall1 + $tall2;
+$differanse = $tall1 - $tall2;
+echo "Tall 1 er $tall1 <br>";
+echo "Tall 2 er $tall2 <br><br>";
+
+echo "Summen er $sum <br>";
+echo "Differansen er $differanse <br>";
+
+?>
